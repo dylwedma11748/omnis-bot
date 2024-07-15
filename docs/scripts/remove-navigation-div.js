0c6ac2b3cc1d90a31fe9889d3b82a9e443b60143
Dylan Wedman <32218999+dylwedma11748@users.noreply.github.com>
2024-07-15 15:48:19 -0500

    Update remove-navigation-div.js

--- docs/scripts/remove-navigation-div.js
+++ docs/scripts/remove-navigation-div.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.pathname === '/' || window.location.pathname === '/commands/' || window.location.pathname === '/pricing/') {
+    if (window.location.pathname === '/' || window.location.pathname === '/index/' || window.location.pathname === '/commands/' || window.location.pathname === '/pricing/') {
         const sidebar = document.querySelector('.md-sidebar--primary');
 		
         if (sidebar) {
