b44abdb3d12d5e266df4e1f59ebde1ac1e37a2c0
dylwedma11748 <32218999+dylwedma11748@users.noreply.github.com>
2024-07-15 20:46:34 +0000

    Deploying to gh-pages from @ dylwedma11748/omnis-bot@568d683bbac8e616877e8a7702525fb8c5addc36 🚀

---
+++ scripts/remove-navigation-div.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.pathname === '/' || window.location.pathname === '/commands/' || window.location.pathname === '/pricing/') {
+        const sidebar = document.querySelector('.md-sidebar--primary');
+		
+        if (sidebar) {
+            sidebar.remove();
+        }
+    }
+});
