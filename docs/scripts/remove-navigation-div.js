6718bd33e0ad9d601ff55b4519386ebe72541bed
Dylan Wedman <32218999+dylwedma11748@users.noreply.github.com>
2024-07-15 23:38:42 -0500

    Update remove-navigation-div.js

--- docs/scripts/remove-navigation-div.js
+++ docs/scripts/remove-navigation-div.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.pathname === '/' || window.location.pathname === '/index' || window.location.pathname === '/commands/' || window.location.pathname === '/pricing/') {
-        const sidebar = document.querySelector('.md-sidebar--primary');
-		
-        if (sidebar) {
-            sidebar.remove();
+    if (window.innerWidth >= 768) {
+        if (window.location.pathname === '/' || window.location.pathname === '/index' || window.location.pathname === '/commands/' || window.location.pathname === '/pricing/') {
+            const sidebar = document.querySelector('.md-sidebar--primary');
+            if (sidebar) {
+                sidebar.remove();
+            }
         }
     }
 });
